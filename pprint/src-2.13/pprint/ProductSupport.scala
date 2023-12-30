35aa75dac29b2673e4334a18bb764edb979f2d78
Flavio Brasil <fwbrasil@gmail.com>
2023-12-29 17:49:36 -0800

    fix rendering of field names with backticks

--- pprint/src-2.13/pprint/ProductSupport.scala
+++ pprint/src-2.13/pprint/ProductSupport.scala
@@ -2,6 +2,17 @@ package pprint
 
 object ProductSupport {
 
+  private def isIdentifier(name: String) = {
+    def isStart(c: Char) = (c == '_') || (c == '$') || Character.isUnicodeIdentifierStart(c)
+    def isPart(c: Char) = (c == '$') || Character.isUnicodeIdentifierPart(c)
+    name.toList match {
+      case first :: rest if(isStart(first)) =>
+        rest.forall(isPart)
+      case _ =>
+        false
+    }
+  }
+
   def treeifyProductElements(x: Product,
                              walker: Walker,
                              escapeUnicode: Boolean,
@@ -11,8 +22,14 @@ object ProductSupport {
       .zipWithIndex
       .map {
         case (name, i) =>
+          val key = 
+            if(!isIdentifier(name)) {
+              s"`$name`"
+            } else {
+              name
+            }
           val elem = x.productElement(i)
-          Tree.KeyValue(name, walker.treeify(elem, escapeUnicode, showFieldNames))
+          Tree.KeyValue(key, walker.treeify(elem, escapeUnicode, showFieldNames))
       }
   }
 
