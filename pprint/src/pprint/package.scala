2e6131b735e4c53b0eac013d9b4259858aac33f0
Ondra Pelech <ondra.pelech@gmail.com>
2025-06-15 08:21:29 +0200

    Add pretty-printing string interpolators for BlackWhite and Color (#151)
    I use PPrint pretty often for logs and this will make (hopefully not
    only) my life easier so I can just write
    ```scala
    bw"A thing $a happened for $b"
    ```
    instead of
    ```scala
    s"A thing ${BlackWhite(a)} happened for ${BlackWhite(b)}"
    ```

--- pprint/src/pprint/package.scala
+++ pprint/src/pprint/package.scala
@@ -8,4 +8,11 @@ package object pprint extends PPrinter{
   def tprint[T: TPrint](implicit config: TPrintColors) = {
     implicitly[TPrint[T]].render
   }
+
+  implicit class BlackWhiteContext(private val self: StringContext) extends AnyVal {
+    def bw(args: Any*): String = PPrinter.BlackWhite.interpolate(self, args)
+  }
+  implicit class ColorContext(private val self: StringContext) extends AnyVal {
+    def pp(args: Any*): String = PPrinter.Color.interpolate(self, args)
+  }
 }
